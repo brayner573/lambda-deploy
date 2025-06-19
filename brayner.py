e1765a475ff2571777df321e29c2b45c935ca3d8
tu-nombre-de-usuario <braynermc2021@gmail.com>
2025-06-19 18:44:54 -0500

    brayner

--- brayner.py
+++ brayner.py
@@ -1,14 +1,1 @@
-# sample 001
-
-# name: test actions
-
-# on:
-# push:
-# branches: [master]
-
-# jobs:
-# test-actions:
-# runs-on: ubuntu-latest
-# steps:
-# - name: Hello
-# run: echo "hello pepelepu or git actions"
+print("brayner anibal amamni calcina")

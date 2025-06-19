2ae846565edc3dfcaf28d518199117ea46ae6af1
tu-nombre-de-usuario <braynermc2021@gmail.com>
2025-06-18 19:14:41 -0500

    Prueba definitiva estave xd

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': '¡Despliegue exitoso desde GitHub Actions!'
+        'body': '¡Despliegue exitoso desde GitHub SA S3 Actions!'
     }

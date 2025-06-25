0a361c43381031908651027a9b34356b423e4cd4
olger856 <infaccallo@gmail.com>
2025-06-24 23:51:34 -0500

    prueba9

--- main.tf
+++ main.tf
@@ -25,6 +25,28 @@ data "aws_iam_role" "lambda_exec" {
   name = "lambda_exec_role"
 }
 
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::upeu-producer-x-bucket-1"
+}
+
+resource "aws_s3_bucket_notification" "lambda_trigger" {
+  bucket = "upeu-producer-x-bucket-1"
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "uploads/"
+    filter_suffix       = ".xlsx"
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3]
+}
+
+
 # Lambda function 
 resource "aws_lambda_function" "lambda" {
   function_name    = var.lambda_name
